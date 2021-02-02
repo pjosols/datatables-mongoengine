ca0e1e90f841c5bc79acdca257950383302f4dcb
Paul Olsen <polsen@diligent.com>
2021-02-02 16:26:07 -0500

    update folder name

--- setup.py
+++ setup.py
@@ -2,26 +2,29 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
-setup(name='datatables_mongoengine',
-      version='0.1.4',
-      description='Mixin for connecting DataTables to MongoDB with MongoEngine.',
-      long_description=readme(),
-      url='https://github.com/pauljolsen/mongoengine-datatables',
-      author='Paul Olsen',
-      author_email='paul@wholeshoot.com',
-      license='MIT',
-      packages=['mongoengine_datatables'],
-      install_requires=['mongoengine'],
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
-          'Topic :: Database :: Database Engines/Servers',
-      ],
-      keywords='flask django mongoengine mongodb',
-      include_package_data=True,
-      zip_safe=False
-      )
+
+setup(
+    name="datatables_mongoengine",
+    version="0.1.4",
+    description="Mixin for connecting DataTables to MongoDB with MongoEngine.",
+    long_description=readme(),
+    url="https://github.com/pauljolsen/mongoengine-datatables",
+    author="Paul Olsen",
+    author_email="paul@wholeshoot.com",
+    license="MIT",
+    long_description_content_type="text/markdown",
+    packages=["datatables_mongoengine"],
+    install_requires=["mongoengine"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Database :: Database Engines/Servers",
+    ],
+    keywords="flask django mongoengine mongodb",
+    include_package_data=True,
+    zip_safe=False,
+)
